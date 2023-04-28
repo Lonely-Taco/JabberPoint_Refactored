82db13cafefc35f67e049703f14a4e7ab775a356
Lonely-Taco <47434636+Lonely-Taco@users.noreply.github.com>
2023-04-28 19:09:18 +0200

    removing unused catch.

--- src/Classes/JabberPoint.java
+++ src/Classes/JabberPoint.java
@@ -4,7 +4,8 @@ import javax.swing.JOptionPane;
 
 import java.io.IOException;
 
-/** Classes.JabberPoint Main Program
+/**
+ * Classes.JabberPoint Main Program
  * <p>This program is distributed under the terms of the accompanying
  * COPYRIGHT.txt file (which is NOT the GNU General Public License).
  * Please read it. Your use of the software constitutes acceptance
@@ -16,31 +17,28 @@ import java.io.IOException;
  * @version 1.4 2007/07/16 Sylvia Stuurman
  * @version 1.5 2010/03/03 Sylvia Stuurman
  * @version 1.6 2014/05/16 Sylvia Stuurman
+ * @version 1.7 2023/04/28 Sylvia Stuurman
  */
 
 public class JabberPoint {
-	protected static final String IOERR = "IO Error: ";
-	protected static final String JABERR = "Jabberpoint Error ";
-	protected static final String JABVERSION = "Jabberpoint 1.6 - OU version";
+    protected static final String IOERR = "IO Error: ";
+    protected static final String JABERR = "Jabberpoint Error ";
+    protected static final String JABVERSION = "Jabberpoint 1.6 - OU version";
 
-	/** The main program */
-	public static void main(String[] argv) {
-		Presentation presentation = new Presentation();
+    /**
+     * The main program
+     */
+    public static void main(String[] argv) {
+        Presentation presentation = new Presentation();
+        new SlideViewerFrame(JABVERSION, presentation);
+        Accessor xmlAccessor = new Accessor();
 
-		new SlideViewerFrame(JABVERSION, presentation);
-		try {
-			if (argv.length == 0) { //a demo presentation
-//				SlideViewerFrame filePicker = new SlideViewerFrame("pick a file", presentation);
-				DemoPresentation demoPresentation = new DemoPresentation();
-				demoPresentation.loadFile(presentation, "demoPresentation.xml");
-			} else {
-				new XMLAccessor().loadFile(presentation, argv[0]);
-			}
-			presentation.setSlideNumber(0);
-		} catch (IOException ex) {
-			JOptionPane.showMessageDialog(null,
-					IOERR + ex, JABERR,
-					JOptionPane.ERROR_MESSAGE);
-		}
-	}
+        if (argv.length == 0) { //a demo presentation
+            xmlAccessor.accessFile(presentation, "demoPresentation.xml");
+        }
+        else {
+            xmlAccessor.accessFile(presentation, argv[0]);
+        }
+        presentation.setSlideNumber(0);
+    }
 }
